5c842f641f6169e38e50b21d74222e3102046d0d
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2023-02-28 12:58:33 +0100

    Project base

---
+++ README.md
@@ -0,0 +1,23 @@
+# Code in the Dark x SensioLabs
+## Installation
+For easier development process, the use of tne Symfony CLI is recommended and all commands are presented as such.
+Remove `symfony` for composer commands, and replace `symfony console` by `php bin/console` for console commands.
+* `git clone` this repository
+* `cd citd-aperodev`
+* `symfony composer install`
+* `yarn install`
+* `yarn encore dev`
+
+This application is made to work with SQLite. Please change the `DATABASE_URL` value in `.env` file to match yours.
+* `symfony console doctrine:database:create`
+
+If you are not using SQLITE, please remove all files in `./migrations`.
+Then, `symfony console doctrine:migrations:diff` before running the rest of the commands.
+* `symfony console doctrine:migrations:migrate`
+* `symfony console doctrine:fixtures:load`
+
+If using the Symfony CLI, you can start the dev server with `symfony serve -d`.
+Otherwise, you can start PHP dev server with `php -S 127.0.0.1:8000 -t public/`
+
+You can visualize the full result of the exercise immediately.
+To give a go at the challenge yourself, `git checkout 4f458edf95c38482a66e57e04d637c2e98d13267`
---
+++ src/Entity/Comment.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CommentRepository::class)]
+class Comment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $postedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'comments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Movie $movie = null;
+
+    #[ORM\Column]
+    private ?int $rating = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPostedAt(): ?\DateTimeImmutable
+    {
+        return $this->postedAt;
+    }
+
+    public function setPostedAt(\DateTimeImmutable $postedAt): self
+    {
+        $this->postedAt = $postedAt;
+
+        return $this;
+    }
+
+    public function getMovie(): ?Movie
+    {
+        return $this->movie;
+    }
+
+    public function setMovie(?Movie $movie): self
+    {
+        $this->movie = $movie;
+
+        return $this;
+    }
+
+    public function getRating(): ?int
+    {
+        return $this->rating;
+    }
+
+    public function setRating(int $rating): self
+    {
+        $this->rating = $rating;
+
+        return $this;
+    }
+}
